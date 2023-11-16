a3b9b5849ecc0f20e138b1e715e95c39f270f006
_omondii <143301363+Benji044@users.noreply.github.com>
2023-11-16 21:53:27 +0300

    Update Library.java

--- Library.java
+++ Library.java
@@ -3,13 +3,13 @@ public class Library{
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int choice = 0;
-
+        
         System.out.println("******** Welcome to the Library Catalog ********");
         System.out.println("1: Add new book");
         System.out.println("2:Display all books ");
         System.out.println("5: Exit");
         System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
         System.out.print("Enter book title: ");
         String title = scanner.next();
         System.out.print("Enter book author:Name ");
